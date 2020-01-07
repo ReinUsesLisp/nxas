9640d78902c3362120a29dc01df52604379f8f6b
ReinUsesLisp <reinuseslisp@airmail.cc>
2020-01-07 20:49:20 -0300

    Add EXIT

---
+++ instructions/exit.c
@@ -0,0 +1,30 @@
+#include "helper.h"
+
+static const char *tests[] = {"F",       "LT",      "EQ",  "LE",     "GT",     "NE",     "GE",
+                              "NUM",     "NAN",     "LTU", "EQU",    "LEU",    "GTU",    "NEU",
+                              "GEU",     "T",       "OFF", "LO",     "SFF",    "LS",     "HI",
+                              "SFT",     "HS",      "OFT", "CSM_TA", "CSM_TR", "CSM_MX", "FCSM_TA",
+                              "FCSM_TR", "FCSM_MX", "RLE", "RGT",    NULL};
+
+DEFINE_INSTRUCTION(exiti)
+{
+    add_bits(instr, 0xE300000000000000ULL);
+
+    struct token token = tokenize(ctx);
+    if (equal(&token, ".KEEPREFCOUNT")) {
+        add_bits(instr, 1ULL << 5);
+        token = tokenize(ctx);
+    }
+
+    uint64_t test_index = 15;
+    if (equal(&token, "CC")) {
+        token = tokenize(ctx);
+        if (!find_in_table(&token, tests, ".", &test_index)) {
+            fatal_error(&token, "unexpected test for NOP");
+        }
+        token = tokenize(ctx);
+    }
+    add_bits(instr, test_index);
+
+    check(&token, TOKEN_TYPE_OPERATOR_SEMICOLON);
+}
